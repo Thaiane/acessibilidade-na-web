8320d5077db18e37ce926104c1b7035e42bea128
Thaiane Braga <Thaiane_Braga@gap.com>
2020-08-18 20:04:56 -0300

    Change carousel to use JS

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,28 @@
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
 
+var new0 = document.querySelector('#new0');
+var new1 = document.querySelector('#new1');
+var new2 = document.querySelector('#new2');
+
+new0.style.display = 'block'
+
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
-    
+
+    if (this.getAttribute('data-sliderItem') === '0') {
+      new0.style.display = 'block';
+      new1.style.display = 'none';
+      new2.style.display = 'none';
+    } else if (this.getAttribute('data-sliderItem') === '1') {
+      new0.style.display = 'none';
+      new1.style.display = 'block';
+      new2.style.display = 'none';
+    } else {
+      new0.style.display = 'none';
+      new1.style.display = 'none';
+      new2.style.display = 'block';
+    }
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
