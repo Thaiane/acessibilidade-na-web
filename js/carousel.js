4b93d0886f7a1d375c2b7daaa02320778315c76b
Thaiane Braga <Thaiane_Braga@gap.com>
2020-08-18 20:18:35 -0300

    Add message to slider actual in carousel

--- js/carousel.js
+++ js/carousel.js
@@ -6,6 +6,11 @@ var new2 = document.querySelector('#new2');
 
 new0.style.display = 'block'
 
+var indicadorSlideAtual = document.createElement('span');
+indicadorSlideAtual.classList.add('escondeVisualmente');
+indicadorSlideAtual.id = 'escondeVisualmente';
+indicadorSlideAtual.textContent = '(Slide atual)';
+
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
@@ -23,6 +28,9 @@ btns.forEach(function(btn) {
       new1.style.display = 'none';
       new2.style.display = 'block';
     }
+
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
+    this.append(indicadorSlideAtual);
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
